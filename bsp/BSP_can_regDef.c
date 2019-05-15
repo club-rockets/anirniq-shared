fb21f69d8250423e3c015a8577e2886477e06b70
David Bourgault <dav.bour@gmail.com>
2019-05-15 19:20:14 -0400

    Added CAN registers IDs

--- bsp/BSP_can_regDef.c
+++ bsp/BSP_can_regDef.c
@@ -1,81 +1,83 @@
 /*
  * BSP_can_regDef.c
  *
- *  Created on: 17 mars 2019
- *      Author: Marc-Andre Denis
+ * Created on: 17 mars 2019
+ * Author: Marc-Andre Denis
  *
- *	Contains can intervace registers definition
+ * Contains CAN interface registers definition
  *
  */
 
 #include "stdint.h"
 #include "BSP_can_regDef.h"
 
+#define DEF_CAN_REGISTER(name) \
+    { \
+        .name = name, \
+        .lastTick = 0, \
+        .changeCallback, \
+        .data = { 0 } \
+    };
+
 volatile can_reg_t can_emergencyRegisters[] = {
-		//one random value for now
-		{
-				.name = "test",
-				.lastTick = 0,
-				.changeCallback = 0,
-				.data = {0}
-		}
+    DEF_CAN_REGISTER("test")
 };
 
 volatile can_reg_t can_missionRegisters[] = {
-		{
-				.name = "heartBeat",
-				.lastTick = 0,
-				.changeCallback = 0,
-				.data = {0}
-		}
+    DEF_CAN_REGISTER("status"),
+    DEF_CAN_REGISTER("rocket_status"),
+    DEF_CAN_REGISTER("charge_status")
 };
 
 volatile can_reg_t can_communicationRegisters[] = {
-		{
-				.name = "heartBeat",
-				.lastTick = 0,
-				.changeCallback = 0,
-				.data = {0}
-		}
+    DEF_CAN_REGISTER("status"),
+    DEF_CAN_REGISTER("command_sleep"),
+    DEF_CAN_REGISTER("command_eject_drogue"),
+    DEF_CAN_REGISTER("command_eject_main")
 };
 
 volatile can_reg_t can_acquisitionRegisters[] = {
-		{
-				.name = "heartBeat",
-				.lastTick = 0,
-				.changeCallback = 0,
-				.data = {0}
-		}
+    DEF_CAN_REGISTER("status"),
+    DEF_CAN_REGISTER("air_pressure"),
+    DEF_CAN_REGISTER("air_temperature"),
+    DEF_CAN_REGISTER("altitude"),
+    DEF_CAN_REGISTER("vertical_velocity"),
+    DEF_CAN_REGISTER("acceleration_x"),
+    DEF_CAN_REGISTER("acceleration_y"),
+    DEF_CAN_REGISTER("acceleration_z"),
+    DEF_CAN_REGISTER("attitude_x"),
+    DEF_CAN_REGISTER("attitude_y"),
+    DEF_CAN_REGISTER("attitude_z"),
+    DEF_CAN_REGISTER("magnetic_field_x"),
+    DEF_CAN_REGISTER("magnetic_field_y"),
+    DEF_CAN_REGISTER("magnetic_field_z"),
+    DEF_CAN_REGISTER("imu_temperature"),
+    DEF_CAN_REGISTER("gps_lat"),
+    DEF_CAN_REGISTER("gps_lon")
 };
 
 volatile can_reg_t can_motherboardRegisters[] = {
-		{
-				.name = "heartBeat",
-				.lastTick = 0,
-				.changeCallback = 0,
-				.data = {0}
-		},
-		{
-			.name = "batteryVoltage",
-			.lastTick = 0,
-			.changeCallback = 0,
-			.data = {0}
-		}
+    DEF_CAN_REGISTER("status"),
+    DEF_CAN_REGISTER("3v3_voltage"),
+    DEF_CAN_REGISTER("3v3_amperage"),
+    DEF_CAN_REGISTER("5v_voltage"),
+    DEF_CAN_REGISTER("5v_amperage"),
+    DEF_CAN_REGISTER("vbatt_voltage"),
+    DEF_CAN_REGISTER("vbatt_amperage")
 };
 
 volatile can_reg_t* can_registers[] = {
-		can_emergencyRegisters,
-		can_missionRegisters,
-		can_communicationRegisters,
-		can_acquisitionRegisters,
-		can_motherboardRegisters,
+    can_emergencyRegisters,
+    can_missionRegisters,
+    can_communicationRegisters,
+    can_acquisitionRegisters,
+    can_motherboardRegisters,
 };
 
 const uint8_t can_registersSize[] = {
-		sizeof(can_emergencyRegisters)/sizeof(can_reg_t),
-		sizeof(can_missionRegisters)/sizeof(can_reg_t),
-		sizeof(can_communicationRegisters)/sizeof(can_reg_t),
-		sizeof(can_acquisitionRegisters)/sizeof(can_reg_t),
-		sizeof(can_motherboardRegisters)/sizeof(can_reg_t)
+    sizeof(can_emergencyRegisters)/sizeof(can_reg_t),
+    sizeof(can_missionRegisters)/sizeof(can_reg_t),
+    sizeof(can_communicationRegisters)/sizeof(can_reg_t),
+    sizeof(can_acquisitionRegisters)/sizeof(can_reg_t),
+    sizeof(can_motherboardRegisters)/sizeof(can_reg_t)
 };
-
--- bsp/BSP_can_regDef.h
+++ bsp/BSP_can_regDef.h
@@ -1,11 +1,11 @@
 /*
  * BSP_can_regDef.h
  *
- *  Created on: 17 mars 2019
- *      Author: Marc-Andre Denis
+ * Created on: 17 mars 2019
+ * Author: Marc-Andre Denis
  *
- *	Contains can intervace registers definition
- *	To be uset only by BSP_CAN.c
+ * Contains CAN interface registers definition
+ * To be uset only by BSP_CAN.c
  *
  */
 #ifndef _BSP_CAN_REGDEF
@@ -14,35 +14,74 @@
 #include "BSP_CAN.h"
 #include "stdint.h"
 
-#define BOARD_EMERGENCY_ID_SHIFTED		(0<<BOARD_ID_SHIFT)
-#define BOARD_MISSION_ID_SHIFTED		(1<<BOARD_ID_SHIFT)
-#define BOARD_COMMUNICATION_ID_SHIFTED	(2<<BOARD_ID_SHIFT)
-#define BOARD_ACQUISITION_ID_SHIFTED	(3<<BOARD_ID_SHIFT)
-#define BOARD_MOTHERBOARD_ID_SHIFTED 	(4<<BOARD_ID_SHIFT)
+#define BOARD_EMERGENCY_ID_SHIFTED     (0 << BOARD_ID_SHIFT)
+#define BOARD_MISSION_ID_SHIFTED       (1 << BOARD_ID_SHIFT)
+#define BOARD_COMMUNICATION_ID_SHIFTED (2 << BOARD_ID_SHIFT)
+#define BOARD_ACQUISITION_ID_SHIFTED   (3 << BOARD_ID_SHIFT)
+#define BOARD_MOTHERBOARD_ID_SHIFTED   (4 << BOARD_ID_SHIFT)
 
+/** CAN Message IDs **/
+/* Emergency */
 #define CAN_EMERGENCY_TEST_INDEX 0
 
+/* Mission */
+#define CAN_MISSION_STATUS_INDEX        0
+#define CAN_MISSION_ROCKET_STATUS_INDEX 1
+#define CAN_MISSION_CHARGE_STATUS_INDEX 2
+
+/* Communication */
+#define CAN_COMMUNICATION_STATUS_INDEX 0
+
+#define CAN_COMMUNICATION_CONTROL_SLEEP_INDEX        1
+#define CAN_COMMUNICATION_CONTROL_EJECT_DROGUE_INDEX 2
+#define CAN_COMMUNICATION_CONTROL_EJECT_MAIN_INDEX   3
+
+/* Acquisition */
+#define CAN_ACQUISITION_STATUS_INDEX            0
+
+#define CAN_ACQUISITION_AIR_PRESSURE_INDEX    1
+#define CAN_ACQUISITION_AIR_TEMPERATURE_INDEX 2
+
+#define CAN_ACQUISITION_ALTITUDE_INDEX          3
+#define CAN_ACQUISITION_VERTICAL_VELOCITY_INDEX 4
+
+#define CAN_ACQUISITION_ACCELERATION_X_INDEX 5
+#define CAN_ACQUISITION_ACCELERATION_Y_INDEX 6
+#define CAN_ACQUISITION_ACCELERATION_Z_INDEX 7
+
+#define CAN_ACQUISITION_ATTITUDE_X_INDEX 8
+#define CAN_ACQUISITION_ATTITUDE_Y_INDEX 9
+#define CAN_ACQUISITION_ATTITUDE_Z_INDEX 10
+
+#define CAN_ACQUISITION_MAGNETIC_FIELD_X_INDEX 11
+#define CAN_ACQUISITION_MAGNETIC_FIELD_Y_INDEX 12
+#define CAN_ACQUISITION_MAGNETIC_FIELD_Z_INDEX 13
+
+#define CAN_ACQUISITION_IMU_TEMPERATURE_INDEX 14
+
+#define CAN_ACQUISITION_GPS_LAT_INDEX 15
+#define CAN_ACQUISITION_GPS_LON_INDEX 16
+
+/* Motherboard */
+#define CAN_MOTHERBOARD_STATUS_INDEX 0
+
+#define CAN_MOTHERBOARD_3V3_VOLTAGE  1
+#define CAN_MOTHERBOARD_3V3_AMPERAGE 2
+
+#define CAN_MOTHERBOARD_5V_VOLTAGE  3
+#define CAN_MOTHERBOARD_5V_AMPERAGE 4
+
+#define CAN_MOTHERBOARD_VBATT_VOLTAGE  5
+#define CAN_MOTHERBOARD_VBATT_AMPERAGE 6
+
 extern volatile can_reg_t can_emergencyRegisters[];
-
-#define CAN_MISSION_HEARTBEAT_INDEX 0
-
 extern volatile can_reg_t can_missionRegisters[];
-
-#define CAN_COMMUNICATION_HEARTBEAT_INDEX 0
-
 extern volatile can_reg_t can_communicationRegisters[];
-
-#define CAN_ACQUISITION_HEARTBEAT_INDEX 0
-
 extern volatile can_reg_t can_acquisitionRegisters[];
-
-#define CAN_MOTHERBOARD_HEARTBEAT_INDEX 0
-
 extern volatile can_reg_t can_motherboardRegisters[];
 
 extern volatile can_reg_t* can_registers[];
 
 extern const uint8_t can_registersSize[];
-
 
 #endif //_BSP_CAN_REGDEF
