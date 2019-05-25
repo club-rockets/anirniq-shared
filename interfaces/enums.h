a827b089b0f26d01cf1355fcd4aaff37e7ab49c1
Hugo Lemoine <hugo.lemoine-st-andre.1@ens.etsmtl.ca>
2019-05-24 20:52:22 -0400

    Add shared rocket definition

---
+++ interfaces/enums.h
@@ -0,0 +1,29 @@
+/*
+ * enums.h
+ *
+ *  Created on: May 24, 2019
+ *      Author: Hugo Lemoine
+ */
+
+#ifndef INTERFACES_ENUMS_H_
+#define INTERFACES_ENUMS_H_
+
+
+typedef enum rocket_state {
+	INITIALISATION,
+	STANDBY_ON_PAD,
+	LAUNCH,
+	POWERED_ASCENT,
+	ENGINE_BURNOUT,
+	COASTING_ASCENT,
+	APOGEE_REACHED,
+	DROGUE_DEPLOYMENT,
+	DROGUE_DESCENT,
+	MAIN_DEPLOYMENT,
+	MAIN_DESCENT,
+	LANDING,
+	RECOVERY,
+	PICKEDUP} rocket_state;
+
+
+#endif /* INTERFACES_ENUMS_H_ */
---
+++ interfaces/id.h
@@ -0,0 +1,15 @@
+/*
+ * id.h
+ *
+ *  Created on: May 24, 2019
+ *      Author: Hugo Lemoine
+ */
+
+#ifndef INTERFACES_ID_H_
+#define INTERFACES_ID_H_
+
+
+
+
+
+#endif /* INTERFACES_ID_H_ */
